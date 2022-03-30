d253589e1076af8c3ee38c1fd5785ccb2aea8409
epshita-shima <epshitashima@gmail.com>
2022-03-30 11:31:04 +0600

    edit for hide props and usestate description

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Products from './Component/Products/Products';
 
-import Props from './Component/Props/Props';
-import UseState from './Component/UseState/UseState';
+// import Props from './Component/Props/Props';
+// import UseState from './Component/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
-      <h2>Fruits and Vegetable Store</h2>
+      <h2 className='heading'>Fruits and Vegetable Store</h2>
       <Products></Products>
-      <Props></Props>
-      <UseState></UseState>
+      {/* <Props></Props>
+      <UseState></UseState> */}
     </div>
   );
 }
--- src/Component/Props/Props.js
+++ src/Component/Props/Props.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './Props.css';
+// import React from 'react';
+// import './Props.css';
 
-const Props = () => {
-    return (
-        <div className='props'>
-            <h2>Props Vs State</h2>
-            <p>In props the Data is passed from one component to another, otherside state passed data within the component only. Props cannot be modified that is call immutable , but state can be modified that called mutable. Also props can be used with state and functional components, Otherhand state can be used only with the state components/class component. Props are read-only and 	State is both read and write.</p>
-        </div>
-    );
-};
+// const Props = () => {
+//     return (
+//         <div className='props'>
+//             <h2>Props Vs State</h2>
+//             <p>In props the Data is passed from one component to another, otherside state passed data within the component only. Props cannot be modified that is call immutable , but state can be modified that called mutable. Also props can be used with state and functional components, Otherhand state can be used only with the state components/class component. Props are read-only and 	State is both read and write.</p>
+//         </div>
+//     );
+// };
 
-export default Props;
+// export default Props;
